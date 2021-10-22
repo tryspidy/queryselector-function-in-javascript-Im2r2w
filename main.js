eeda63ce6a1104eb08c4d6b90585753fcdb3305a
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-22 20:12:56 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/*The querySelector() method returns the first element that matches a specified 
+CSS selector(s) in the document. Note: The querySelector() method only returns 
+the first element that matches the specified selectors. To return all the 
+matches, use the querySelectorAll() method instead.*/
+
+// for example 
+
+//for class
+document.querySelector(".ClassName")
+
+// for id 
+document.querySelector("#ID")
+
+// etc.
